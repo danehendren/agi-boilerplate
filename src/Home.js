979158a7ca36bea07b47388431d7d65d06e7b04e
D <speakdane@gmail.com>
2018-03-20 17:31:41 +0100

    home page added improperly - needs changes

--- src/Home.js
+++ src/Home.js
@@ -6,18 +6,18 @@ function Home(props) {
     return (
         <div className="container">
 
-            <div className="left-half-home">
+            <section className="left-half-home">
                 <p>This is the left side.</p>
                 <img src="media/cat-photo.jpg" className="about-sm-img"  alt="" />
                 <img src="media/cat-photo.jpg" className="about-sm-img"  alt="" />
                 <img src="media/cat-photo.jpg" className="about-sm-img"  alt="" />
                 <img src="media/cat-photo.jpg" className="about-sm-img"  alt="" />
-            </div>
+            </section>
 
-            <div className="right-half-home">
+            <section className="right-half-home">
                 <p>side Right the is this.</p>
                 <img src="media/cat-photo.jpg" className="about-sm-img"  alt="" />
-            </div>
+            </section>
 
         </div>
     )
--- src/Journey.js
+++ src/Journey.js
@@ -5,18 +5,30 @@ function Journey(props) {
 
     return (
         <div className="container">
-
             <div className="left-half-journey">
                 <p>This is the left side.</p>
-                <img src="media/cat-photo.jpg" className="about-sm-img"  alt="" />
-                <img src="media/cat-photo.jpg" className="about-sm-img"  alt="" />
-                <img src="media/cat-photo.jpg" className="about-sm-img"  alt="" />
-                <img src="media/cat-photo.jpg" className="about-sm-img"  alt="" />
+                    <img src="media/cat-photo.jpg" className="journey-md-img"  alt="" />
+                    <img src="media/cat-photo.jpg" className="journey-md-img"  alt="" />
+                    <img src="media/cat-photo.jpg" className="journey-md-img"  alt="" />
             </div>
 
             <div className="right-half-journey">
-                <p>side Right the is this.</p>
-                <img src="media/cat-photo.jpg" className="about-sm-img"  alt="" />
+                    <div className="journey-block">
+                        <h2>A little bit of this and a little bit of that</h2>
+                        <p>Beginning a new life in Nashville, TN I was hoping to find a different/better/more interesting career.  </p>
+                    </div>
+                    <div className="journey-block">
+                        <h2>Marriage and Moving</h2>
+                        <p>Beginning with my wife and I getting married in Nashville, Tennesse I was out for something different than before.</p>
+                    </div>
+                    <div className="journey-block">
+                        <h2>2017 Begins...</h2>
+                        <p>Being in Germany for a few months I had continued looking for a job.  Finding a profession in what I had done wasn't what I was looking for.  I wanted something new but I wasn't sure.  I thought back to my friends bootcamp journey and my days of giving it a shot.</p>
+                    </div>
+                    <div className="journey-block">
+                        <h2>To Berlin and Beyond...</h2>
+                        <p>There are words coming here to explain SPICED Academy and then the after the fact times</p>
+                    </div>
             </div>
 
         </div>
