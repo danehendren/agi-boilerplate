feedbacb845c437a1adff6202fc877bfd218c6b4
D <speakdane@gmail.com>
2018-03-28 00:15:50 +0200

    Fiddling around with cursor js

--- src/Home.js
+++ src/Home.js
@@ -3,6 +3,26 @@ import React from 'react';
 
 function Home(props) {
 
+
+
+        function handleClick(e) {
+            e.preventDefault();
+            console.log('This was clicked, yay!');
+
+            // if(true) {
+            //     var followMe = document.getElementById('mouse');
+            //     var box = document.getElementById('exercise1');
+            //
+            //     document.addEventListener("mousemove", function(e){
+            //
+            //         box.style.left = e.clientX - 50 + 'px';
+            //         box.style.top = e.clientY - 50 + 'px';
+            //     });
+            // }
+        }
+
+
+
     return (
         <div className="container">
 
@@ -13,8 +33,8 @@ function Home(props) {
                     <img src="media/foxfox.jpg" className="about-sm-img"  alt="" />
 
                 <div className="journey-block">
-                    <h2>Petition for Dog Park in Bremen</h2>
-                    <p></p>
+                    <h2 onClick={handleClick} >Petition for Dog Park in Bremen</h2>
+                    <p>Click Me!</p>
                 </div>
 
                 <img src="screenshots/foxSCREENSHOThome.png" className="about-sm-img"  alt="" />
